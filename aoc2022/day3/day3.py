995ba2a748103c2f9eccd5b04c0310769b5838a4
Tjhohn <83258854+Tjhohn@users.noreply.github.com>
2022-12-16 20:03:04 -0700

    finished day3

--- aoc2022/day3/day3.py
+++ aoc2022/day3/day3.py
@@ -1,12 +1,20 @@
 def day3():
     sum = 0
+    sum2 = 0
     with open('aoc2022/day3/input') as f:
         lines = f.readlines()
         for line in lines:
             sum += line_sum(line)
-            print(sum)
 
-    return sum
+        for i in range(len(lines)//3):
+            line1 = lines[i *3 ].strip()
+            line2 = lines[i*3 +1]
+            line3 = lines[i*3 +2]
+            sum2 += getPriority(getMatch( line1, line2, line3))
+
+
+
+    return sum, sum2
         
 def line_sum(line):
     part1 = line[:len(line)//2]
@@ -16,6 +24,10 @@ def line_sum(line):
         for c in part2:
             if c == part1[i]:
                 return getPriority(c)
+
+def getMatch(line1, line2, line3):
+    match = list(set(line1).intersection(line2,line3))
+    return match[0]
 
 def getPriority(c):
     if ord(c) - 96 > 0:
