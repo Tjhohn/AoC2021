82f56207ade06d8143e51b77bfbd48bca03a5fe2
Tanner Hohn <83258854+Tjhohn@users.noreply.github.com>
2022-12-19 09:20:42 -0700

    start day7

--- aoc2022/day7/day7.py
+++ aoc2022/day7/day7.py
@@ -6,6 +6,14 @@ def day7():
         
     return 0
 
+def runCMD(cmd):
+    if cmd[0] == '$':
+        pass
+    elif cmd[0:2] == 'dir':
+        pass
+    else:#file size
+        pass
+
 if __name__ == '__main__':
     print("day 7 result")
     print(day7())
