957a19b11675043b970e7f26c2c580b69c2236bd
Tjhohn <83258854+Tjhohn@users.noreply.github.com>
2022-12-20 21:31:25 -0700

    day7 part 1

--- aoc2022/day7/day7.py
+++ aoc2022/day7/day7.py
@@ -1,18 +1,45 @@
+directories = {'/':0}
+path = '/'
+
 def day7():
     with open('aoc2022/day7/input') as f:
         lines = f.readlines()
         for line in lines:
-            pass
-        
-    return 0
+            processCMD(line)
 
-def runCMD(cmd):
+    total = 0
+
+    for directory in directories:
+        if directories[directory] < 100000:
+            total += directories[directory]
+
+        
+    return total
+
+def processCMD(cmd):
+    global path
     if cmd[0] == '$':
-        pass
-    elif cmd[0:2] == 'dir':
-        pass
-    else:#file size
-        pass
+        if cmd[2:4] == 'ls':
+            pass # need later?
+        elif cmd[2:4] == 'cd':
+            if cmd[5:6] == '/':
+                path = '/'
+            elif cmd[5:7] == '..':
+                path = path[0:path.rfind('/')]
+            else: 
+                directory_name = cmd.split()[2]
+                print(directory_name)
+                path = path + '/' + directory_name
+                directories[path] = 0
+    elif cmd[0:3] == 'dir':
+        pass # mau need later
+    else: #file size
+        file_info = cmd.split()
+        directory = path
+        for i in range(path.count('/')):
+            directories[directory] += int(file_info[0])
+            directory= directory[:directory.rfind('/')]
+
 
 if __name__ == '__main__':
     print("day 7 result")
