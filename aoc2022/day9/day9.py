50dff3601362492eae697300726fb1446b563261
Tjhohn <83258854+Tjhohn@users.noreply.github.com>
2022-12-23 00:29:31 -0700

    day9 initial

---
+++ aoc2022/day9/day9.py
@@ -0,0 +1,10 @@
+def day9():
+    with open('aoc2022/day9/input') as f:
+        lines = f.readlines()
+        for line in lines:
+            pass
+   
+    
+if __name__ == '__main__':
+    print("day 9 result")
+    day9()
