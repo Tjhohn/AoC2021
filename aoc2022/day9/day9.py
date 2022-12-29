5e907fd389e17d2bfcc88566de5e88d1a0334196
Tjhohn <83258854+Tjhohn@users.noreply.github.com>
2022-12-28 19:01:06 -0700

    solved day9 sarted 10

--- aoc2022/day9/day9.py
+++ aoc2022/day9/day9.py
@@ -1,4 +1,4 @@
-def day9():
+def day9part1():
     tail_visited = set()
     tail_visited.add((0,0))
     head = [0, 0] # x, y
@@ -6,12 +6,67 @@ def day9():
     with open('aoc2022/day9/input') as f:
         lines = f.readlines()
         for line in lines:
-            head, tail = Action(head, tail, tail_visited, line)
+            head, tail = Action_p1(head, tail, tail_visited, line)
 
-    print(len(tail_visited)) #133 is wrong, 331 too low
+    print(len(tail_visited))
+    
+def day9part2():
+    tail_visited = set()
+    tail_visited.add((0,0))
+    knots = [[0,0]]*10 # x, y
+    print(knots)
+    with open('aoc2022/day9/input') as f:
+        lines = f.readlines()
+        for line in lines:
+            knots = Action_p2(knots, tail_visited, line)
+
+    print(len(tail_visited)) #761 too low
             
 
-def Action(head, tail, set, input):
+def Action_p2(knots, set, input):
+    direction, steps = input.split()
+    steps = int(steps)
+    match direction:
+        case 'R':
+            for i in range(0, steps):
+                knots[0] = (knots[0][0]+1, knots[0][1])
+                for j in range(1, 10): #check and update each knot
+                    if not checkTail(knots[j-1], knots[j]):
+                        knots[j] = updateTail(knots[j-1], knots[j])
+                        set.add((knots[-1][0],knots[-1][1]))
+                        
+            print('R : knots:', knots)
+        case 'U':
+            for i in range(0, steps ):
+                knots[0] = (knots[0][0], knots[0][1]+1)
+                for j in range(1, 10): #check and update each knot
+                    if not checkTail(knots[j-1], knots[j]):
+                        knots[j] = updateTail(knots[j-1], knots[j])
+                        set.add((knots[-1][0],knots[-1][1]))
+
+            print('U : knots:', knots)
+        case 'L': 
+            for i in range(0, steps ):
+                knots[0] = (knots[0][0]-1, knots[0][1])
+                for j in range(1, 10): #check and update each knot
+                    if not checkTail(knots[j-1], knots[j]):
+                        knots[j] = updateTail(knots[j-1], knots[j])
+                        set.add((knots[-1][0],knots[-1][1]))
+
+            print('L : knots:', knots)
+        case 'D':
+            for i in range(0, steps):
+                knots[0] = (knots[0][0], knots[0][1]-1)
+                for j in range(1, 10): #check and update each knot
+                    if not checkTail(knots[j-1], knots[j]):
+                        knots[j] = updateTail(knots[j-1], knots[j])
+                        set.add((knots[-1][0],knots[-1][1]))
+
+            print('D : knots:', knots)
+
+    return knots
+
+def Action_p1(head, tail, set, input):
     direction, steps = input.split()
     steps = int(steps)
     match direction:
@@ -22,7 +77,7 @@ def Action(head, tail, set, input):
                     tail = updateTail(head, tail)
                     set.add((tail[0],tail[1]))
             
-            print('R : head :', head, 'tail :', tail)
+            # print('R : head :', head, 'tail :', tail)
         case 'U':
             for i in range(0, steps ):
                 head[1] = head[1] + 1
@@ -30,15 +85,15 @@ def Action(head, tail, set, input):
                     tail = updateTail(head, tail)
                     set.add((tail[0],tail[1]))
 
-            print('U : head :', head, 'tail :', tail)
-        case 'L': #fixme
+            # print('U : head :', head, 'tail :', tail)
+        case 'L':
             for i in range(0, steps ):
                 head[0] = head[0] -1
                 if not checkTail(head, tail):
                     tail = updateTail(head, tail)
                     set.add((tail[0],tail[1]))
 
-            print('L : head :', head, 'tail :', tail)
+            # print('L : head :', head, 'tail :', tail)
         case 'D':
             for i in range(0, steps):
                 head[1] = head[1] -1
@@ -46,10 +101,9 @@ def Action(head, tail, set, input):
                     tail = updateTail(head, tail)
                     set.add((tail[0],tail[1]))
 
-            print('D : head :', head, 'tail :', tail)
-            
-    return head, tail
+            # print('D : head :', head, 'tail :', tail)
 
+    return head, tail
 
 def checkTail(head, tail):
     safe_pos = {(tail[0]-1, tail[1]-1), (tail[0], tail[1]-1), (tail[0]+1, tail[1]-1),
@@ -58,14 +112,14 @@ def checkTail(head, tail):
 
     return (head[0],head[1]) in safe_pos
 
-def updateTail(head, tail): # [0,3] and [1,1] -> ret [0,2] not [0,1]
-    diff_x = head[0] - tail[0]
-    diff_y = head[1] - tail[1]
+def updateTail(head, tail): 
+    diff_x = (head[0] - tail[0])
+    diff_y = (head[1] - tail[1])
     if abs(diff_x) > 1 or abs(diff_y) > 1:
-        return [tail[0] + sign(diff_x), tail[1] + sign(diff_y)]
+        return [tail[0] + checkSign(diff_x), tail[1] + checkSign(diff_y)]
     return tail
 
-def sign( num : int ):
+def checkSign( num : int ):    
     if num < 0:
         return -1
     elif num == 0:
@@ -75,5 +129,5 @@ def sign( num : int ):
 
 if __name__ == '__main__':
     print("day 9 result")
-    day9()
-    print(updateTail([0,3], [1,1]))
+    day9part1()
+    day9part2()
