880b2d99f0b0024b5d128ff1d113e5bbc1d8b673
Tjhohn <83258854+Tjhohn@users.noreply.github.com>
2023-01-08 22:26:38 -0700

    day 11 solved, parseing is not worth effort

--- main.py
+++ main.py
@@ -16,10 +16,7 @@ def print_hi(name):
     print(45 % 40)
     print(70 % 40)
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print(1501 // 3)
 
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
