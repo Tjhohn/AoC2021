308e4ad8ea872b91dd061379de76531eb4e9dc57
Tjhohn <83258854+Tjhohn@users.noreply.github.com>
2023-12-05 01:16:35 -0700

    day five part 1 done, somehow

--- aoc2023/day5/day5.py
+++ aoc2023/day5/day5.py
@@ -12,17 +12,55 @@ def part1(input):
     print(f"part one : {min(seeds)}")
 
 def part2(input):
-    sum = 0
-    for line in input:
-        pass
+    seeds, maps = parse_input(input)
+    seed_ranges = list(zip(seeds[0::2], seeds[1::2]))
 
-    print(f"part two : {sum}")
+
+    for conversion_map in maps:
+        next_seed_ranges = []
+        for seed_range in seed_ranges:
+            converted_ranges = apply_map_range(seed_range, conversion_map)
+            next_seed_ranges.extend(converted_ranges)
+
+        seed_ranges = next_seed_ranges
+
+    # Ensure that seeds_ranges is always a list of tuples
+    seeds_ranges = [seed_range if isinstance(seed_range, tuple) else (seed_range,) for seed_range in seeds_ranges]
+
+    # Find the lowest location based on the starting value of the seed range
+    lowest_location = min(seeds_ranges, key=lambda x: x[0])
+
+    print(f"part two: {lowest_location[0]}")
+    # for conversion_map in maps:
+    #     next_seed_range = []
+    #     for start, length in zip(seeds_range[0::2], seeds_range[1::2]):
+    #         converted_range = apply_map_range((start, length), conversion_map)
+    #         next_seed_range.extend(converted_range)
+    #
+    #     seeds_range = next_seed_range
+
+    print(f"part two: {min(seeds_ranges)}")
 
 def apply_map(number, map):
     for dest_start, source_start, length in map:
         if source_start <= number < source_start + length:
             return dest_start + (number - source_start)
     return number
+
+# def apply_map_range(range, mapt):
+#     dest_start, source_start, length = mapt
+#     new_start = apply_map(range[0], [(dest_start, source_start, length)])
+#     new_end = apply_map(range[0] + range[1] - 1, [(dest_start, source_start, length)])
+#     return [(new_start, new_end - new_start + 1)]
+
+def apply_map_range(seed_range, maps):
+    for mapt in maps:
+        dest_start, source_start, length = mapt
+        new_start = apply_map(seed_range[0], [(dest_start, source_start, length)])
+        new_end = apply_map(seed_range[0] + seed_range[1] - 1, [(dest_start, source_start, length)])
+        seed_range = (new_start, new_end - new_start + 1)
+
+    return seed_range
 
 def parse_input(input):
     seeds = list(input[0].split(":")[1].strip().split(" "))
